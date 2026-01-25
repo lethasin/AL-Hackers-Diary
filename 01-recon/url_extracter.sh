628298b5418e4677d273e470abcf454f71cc8378
Lethal & ASIN <alphacharlie440@gmail.com>
2026-01-25 07:03:28 -0400

    Create url_extracter.sh
    Script added to extract urls from source code of google search

---
+++ 01-recon/url_extracter.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#Save as url_extractor.sh
+#chmod +x url_extractor.sh
+echo "\nUse this way to save output to a file: \n\turl_extractor.sh <html_file> >> output.txt\n"
+# check argument
+if [ $# -ne 1 ]; then
+  echo "Usage: url_extractor <html_file>"
+  exit 1
+fi
+
+FILE="$1"
+
+if [ ! -f "$FILE" ]; then
+  echo "Error: File not found -> $FILE"
+  exit 1
+fi
+
+read -r -p "Enter allowed TLDs (comma-separated, e.g. in,gov.in,nic.in,com,org): " TLDS
+
+TLD_REGEX=$(echo "$TLDS" | sed 's/,/|/g')
+
+grep -oE '(http|https)://[^"< >]+|www\.[^"< >]+' "$FILE" \
+| sed -E 's#^(http://|https://)##' \
+| sed -E 's/^www\.//' \
+| sed -E 's#[\\/].*##' \
+| sed -E 's/[[:punct:]]+$//' \
+| grep -Ei "\.($TLD_REGEX)$" \
+| sort -u
